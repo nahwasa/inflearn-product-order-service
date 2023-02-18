5e2402467a8a3824a91774557a7038ce1a94d28a
nahwasa <nahwasa@gmail.com>
2023-02-18 14:01:11 +0900

    initializr

---
+++ src/main/java/com/nahwasa/study/inflearnproductorderservice/InflearnProductOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nahwasa.study.inflearnproductorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnProductOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnProductOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nahwasa/study/inflearnproductorderservice/InflearnProductOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwasa.study.inflearnproductorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnProductOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
