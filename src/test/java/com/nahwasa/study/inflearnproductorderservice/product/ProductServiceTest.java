419c631656e5a5de46d6973aa190b62b05eee4cd
nahwasa <nahwasa@gmail.com>
2023-02-22 22:33:29 +0900

    [상품 수정 API 개발] Mockito로 변경

--- src/test/java/com/nahwasa/study/inflearnproductorderservice/product/ProductServiceTest.java
+++ src/test/java/com/nahwasa/study/inflearnproductorderservice/product/ProductServiceTest.java
@@ -3,14 +3,19 @@ package com.nahwasa.study.inflearnproductorderservice.product;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
     private ProductService productService;
-    private StubProductPort productPort = new StubProductPort();
+    private ProductPort productPort;
 
     @BeforeEach
     void setUp() {
+        productPort = Mockito.mock(ProductPort.class);
         productService = new ProductService(productPort);
     }
 
@@ -19,25 +24,11 @@ class ProductServiceTest {
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
         final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        productPort.getProduct_will_return = product;
+        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         productService.updateProduct(productId, request);
 
         Assertions.assertThat(product.getName()).isEqualTo("상품 수정");
         Assertions.assertThat(product.getPrice()).isEqualTo(2000);
-    }
-
-    private static class StubProductPort implements ProductPort {
-        public Product getProduct_will_return;
-
-        @Override
-        public void save(final Product product) {
-
-        }
-
-        @Override
-        public Product getProduct(final Long productId) {
-            return getProduct_will_return;
-        }
     }
 }
