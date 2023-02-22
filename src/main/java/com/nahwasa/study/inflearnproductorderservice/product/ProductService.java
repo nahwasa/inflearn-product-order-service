bbb7a4c50ba69266a901d6d9a828e5436cbe25d7
nahwasa <nahwasa@gmail.com>
2023-02-22 20:03:17 +0900

    [상품등록 API] API 테스트로 전환 (REST-ASSURED 사용)

--- src/main/java/com/nahwasa/study/inflearnproductorderservice/product/ProductService.java
+++ src/main/java/com/nahwasa/study/inflearnproductorderservice/product/ProductService.java
@@ -1,9 +1,14 @@
 package com.nahwasa.study.inflearnproductorderservice.product;
 
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/products")
 class ProductService {
     private final ProductPort productPort;
 
@@ -11,10 +16,12 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    @Transactional
-    public void addProduct(AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
---
+++ src/test/java/com/nahwasa/study/inflearnproductorderservice/product/ProductApiTest.java
@@ -0,0 +1,44 @@
+package com.nahwasa.study.inflearnproductorderservice.product;
+
+import com.nahwasa.study.inflearnproductorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.awt.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ProductApiTest extends ApiTest {
+
+    @Autowired private ProductService productService;
+
+    @Test
+    void 상품등록() {
+        final AddProductRequest request = 상품등록요청_생성();
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all() // 요청을 보내는 로그를 남기겠다
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private static AddProductRequest 상품등록요청_생성() {
+        final String name = "상품명";
+        final int price = 1000;
+        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
+        final AddProductRequest request = new AddProductRequest(name, price, discountPolicy);
+        return request;
+    }
+}
