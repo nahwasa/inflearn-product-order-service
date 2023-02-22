25769a0d5e7c8d68c8c0b5339713736a7b1aa915
nahwasa <nahwasa@gmail.com>
2023-02-22 23:03:14 +0900

    [상품 수정 API 개발] 스프링부트 테스트로 전환

--- src/test/java/com/nahwasa/study/inflearnproductorderservice/product/ProductServiceTest.java
+++ src/test/java/com/nahwasa/study/inflearnproductorderservice/product/ProductServiceTest.java
@@ -1,34 +1,28 @@
 package com.nahwasa.study.inflearnproductorderservice.product;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class ProductServiceTest {
 
-    private ProductService productService;
-    private ProductPort productPort;
-
-    @BeforeEach
-    void setUp() {
-        productPort = Mockito.mock(ProductPort.class);
-        productService = new ProductService(productPort);
-    }
+    @Autowired private ProductService productService;
 
     @Test
     void 상품수정() {
+        productService.addProduct(ProductSteps.상품등록요청_생성());
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
-        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         productService.updateProduct(productId, request);
 
-        Assertions.assertThat(product.getName()).isEqualTo("상품 수정");
-        Assertions.assertThat(product.getPrice()).isEqualTo(2000);
+        final ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        final GetProductResponse productResponse = response.getBody();
+
+        Assertions.assertThat(productResponse.name()).isEqualTo("상품 수정");
+        Assertions.assertThat(productResponse.price()).isEqualTo(2000);
     }
 }
