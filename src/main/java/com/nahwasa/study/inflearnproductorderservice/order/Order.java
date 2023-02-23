6eaebf2ef1ab19a6795799735eeda16af4005732
nahwasa <nahwasa@gmail.com>
2023-02-23 20:52:16 +0900

    [상품 주문 API 개발] Map으로 되있던 레포를 JPA로 변경

--- src/main/java/com/nahwasa/study/inflearnproductorderservice/order/Order.java
+++ src/main/java/com/nahwasa/study/inflearnproductorderservice/order/Order.java
@@ -1,26 +1,28 @@
 package com.nahwasa.study.inflearnproductorderservice.order;
 
 import com.nahwasa.study.inflearnproductorderservice.product.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
-
-    private final Product product;
-    private final int quantity;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
+    private Product product;
+    private int quantity;
 
     public Order(final Product product, final int quantity) {
         Assert.notNull(product, "상품 필수");
         Assert.isTrue(quantity > 0, "수량은 0보다 커야 합니다.");
         this.product = product;
         this.quantity = quantity;
-    }
-
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
--- src/main/java/com/nahwasa/study/inflearnproductorderservice/order/OrderRepository.java
+++ src/main/java/com/nahwasa/study/inflearnproductorderservice/order/OrderRepository.java
@@ -1,17 +1,6 @@
 package com.nahwasa.study.inflearnproductorderservice.order;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Repository
-class OrderRepository {
-    private final Map<Long, Order> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(final Order order) {
-        order.assignId(++sequence);
-        persistence.put(order.getId(), order);
-    }
+interface OrderRepository extends JpaRepository<Order, Long> {
 }
