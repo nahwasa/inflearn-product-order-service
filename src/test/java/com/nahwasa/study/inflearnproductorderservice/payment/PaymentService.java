0653f7444cb78f20f1326177c7eac05b5c54800e
nahwasa <nahwasa@gmail.com>
2023-02-23 22:26:16 +0900

    [주문 결제 API 개발] API 테스트로 전환

--- src/test/java/com/nahwasa/study/inflearnproductorderservice/payment/PaymentService.java
+++ src/test/java/com/nahwasa/study/inflearnproductorderservice/payment/PaymentService.java
@@ -1,9 +1,16 @@
 package com.nahwasa.study.inflearnproductorderservice.payment;
 
 import com.nahwasa.study.inflearnproductorderservice.order.Order;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/payments")
 class PaymentService {
     private final PaymentPort paymentPort;
 
@@ -11,12 +18,16 @@ class PaymentService {
         this.paymentPort = paymentPort;
     }
 
-    public void payment(final PaymentRequest request) {
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Void> payment(@RequestBody final PaymentRequest request) {
         final Order order = paymentPort.getOrder(request.orderId());
 
         final Payment payment = new Payment(order, request.cardNumber());
 
         paymentPort.pay(payment.getPrice(), payment.getCardNumber());
         paymentPort.save(payment);
+
+        return ResponseEntity.ok().build();
     }
 }
